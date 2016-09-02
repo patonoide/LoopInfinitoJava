7ea011ebd9d24ff7415db3eacceaed07ae9e40d4
patonoide <gabrielpoliveira@ig.com.br>
2016-09-01 21:20:43 -0300

    versão final
    versão final loop "infinito"

---
+++ descarrega/src/descarrega/Descarrega.java
@@ -0,0 +1,13 @@
+
+package descarrega;
+
+public class Descarrega {
+    public static void main(String[] args) {
+       short x=-1;
+       while(x!=-2){
+           x=(short) (x+1);
+           System.out.println(x);
+       }
+    }
+    
+}
